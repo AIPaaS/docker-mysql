fc74af53bd597818ddc2aa60b40b8c50d484d2f4
yuanman <yuanman@asiainfo.com>
2016-07-11 18:42:37 +0800

    modify mysql privileges.

--- mysql_start.sh
+++ mysql_start.sh
@@ -5,13 +5,14 @@ doSql=true
 /Percona-Server-5.6.24/bin/mysqld_safe --defaults-file=/Percona-Server-5.6.24/my.cnf --log-error=/Percona-Server-5.6.24/mysql-39316.err &
 
 while(true); do
-  sleep 6s
+  sleep 10s
   if $doSql; then
-	/Percona-Server-5.6.24/bin/mysql -h localhost -u root --socket=/Percona-Server-5.6.24/mysqld-39316.sock</Percona-Server-5.6.24/createDB.sql
-	/Percona-Server-5.6.24/bin/mysql -h localhost -u root --socket=/Percona-Server-5.6.24/mysqld-39316.sock</Percona-Server-5.6.24/service_backend.sql
-	/Percona-Server-5.6.24/bin/mysql -h localhost -u root --socket=/Percona-Server-5.6.24/mysqld-39316.sock</Percona-Server-5.6.24/portal_backend.sql
-	/Percona-Server-5.6.24/bin/mysql -h localhost -u root --socket=/Percona-Server-5.6.24/mysqld-39316.sock</Percona-Server-5.6.24/portal_web.sql
-        doSql=false
+	/Percona-Server-5.6.24/bin/mysqladmin password 123456 -u root --socket=/Percona-Server-5.6.24/mysqld-39316.sock
+	/Percona-Server-5.6.24/bin/mysql -h localhost -u root -p123456 --socket=/Percona-Server-5.6.24/mysqld-39316.sock</Percona-Server-5.6.24/createDB.sql
+	/Percona-Server-5.6.24/bin/mysql -h localhost -u root -p123456 --socket=/Percona-Server-5.6.24/mysqld-39316.sock</Percona-Server-5.6.24/service_backend.sql
+	/Percona-Server-5.6.24/bin/mysql -h localhost -u root -p123456 --socket=/Percona-Server-5.6.24/mysqld-39316.sock</Percona-Server-5.6.24/portal_backend.sql
+	/Percona-Server-5.6.24/bin/mysql -h localhost -u root -p123456 --socket=/Percona-Server-5.6.24/mysqld-39316.sock</Percona-Server-5.6.24/portal_web.sql
+	doSql=false
   fi
 done
 
